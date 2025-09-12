aacef9f9335f0d5ebed07b7090d427dfd983717b
Shuvro882 <sahashuvro74@gmail.com>
2025-09-12 22:08:54 +0600

    add categories tree functionality in this site

--- script/index.js
+++ script/index.js
@@ -4,13 +4,48 @@ const loadCategories = () => {
     .then(json => displayCategories(json.categories));
 };
 
+const loadTreeDetails=(id)=>{
+  
+  const url=`https://openapi.programming-hero.com/api/category/${id}`;
+  fetch(url)
+  .then(res=>res.json())
+  .then(data=>disCategoryTree(data.plants))
+};
+
+const disCategoryTree=(trees)=>{
+    const treeContainer=document.getElementById("cards");
+    treeContainer.innerHTML="";
+
+    trees.forEach(tree=>{
+        console.log(tree);
+        const card = document.createElement("div");
+        card.innerHTML=`
+        <div class="shadow p-3 bg-white rounded-lg">
+        <img src="${tree.image}" alt="${tree.name}" class="w-full object-cover rounded-lg h-[300px]">
+        <h3 onclick="loadTreeDetail(${tree.id})" class="font-semibold mt-2">${tree.name}</h3>
+        <p class="text-sm text-gray-500">${tree.description}</p>
+        <div class="flex justify-between items-center">
+          <button class="text-[#15803D] bg-[#DCFCE7] rounded-full p-1">${tree.category}</button>
+          <p class="font-bold"><i class="fa-solid fa-bangladeshi-taka-sign"></i>${tree.price}</p>
+        </div>
+        <button class="mt-2 w-full bg-green-500 text-white px-3 py-1 rounded-full hover:bg-green-600 add-to-cart">
+          Add to Cart
+        </button>
+      </div>
+        `;
+        treeContainer.append(card);
+    })
+};
+
+
+
 const displayCategories = (categories) => {
     const categoryList = document.getElementById("category-list");
     categoryList.innerHTML = "";
 
     for (let category of categories) {
         const btnUl = document.createElement("ul");
-        btnUl.innerHTML = `<li class="p-2 rounded cursor-pointer hover:bg-green-500">${category.category_name}</li>`;
+        btnUl.innerHTML = `<li onclick="loadTreeDetails('${category.id}')" class="p-2 rounded cursor-pointer hover:bg-green-500">${category.category_name}</li>`;
         categoryList.append(btnUl);
     }
 };
@@ -61,7 +96,7 @@ const displayTrees = (trees) => {
 
         div.innerHTML = `
         <img src="${tree.image}" alt="${tree.name}" class="w-full object-cover rounded-lg h-[300px]">
-        <h3 onclick="loadTreeDetail(${tree.id})" class="font-semibold mt-2">${tree.name}</h3>
+        <h3 onclick="loadTreeDetail(${tree.id})" class="font-semibold mt-2 cursor-pointer">${tree.name}</h3>
         <p class="text-sm text-gray-500">${tree.description}</p>
         <div class="flex justify-between items-center">
           <button class="text-[#15803D] bg-[#DCFCE7] rounded-full p-1">${tree.category}</button>
@@ -73,8 +108,9 @@ const displayTrees = (trees) => {
         `;
         cardContainer.appendChild(div);
 
-        // Add to Cart functionality
-        const addToCartBtn = div.querySelector(".add-to-cart");
+
+// Add to Cart functionality
+const addToCartBtn = div.querySelector(".add-to-cart");
         addToCartBtn.addEventListener("click", () => addToCart(tree));
     });
 };
